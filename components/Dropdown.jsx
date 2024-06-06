0bae0f7a2ff2e746b13d074289ab33209fe4d449
Javeriaamjad <javeriaamjad37@gmail.com>
2024-06-07 01:15:20 +0500

    UI explore

---
+++ components/Dropdown.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const Dropdown = ({ onSelectCategory }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const categories = ['food blogger', 'traveler', 'fashion', 'comedy'];
+
+  const handleCategoryClick = (category) => {
+    onSelectCategory(category);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="relative">
+      <button
+        onClick={toggleDropdown}
+        className="dropbtn bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white font-semibold py-2 px-4 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out"
+      >
+        Categories
+      </button>
+      {isOpen && (
+        <div className="absolute mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            {categories.map((category, index) => (
+              <button
+                onClick={() => handleCategoryClick(category)}
+                key={index}
+                className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-purple-100 hover:text-gray-900 transition-colors duration-200 ease-in-out"
+                role="menuitem"
+              >
+                {category}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
