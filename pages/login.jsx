f2c0c3f5d68e16aaa397cb33cde65474f2277705
marry <marry@gmail.com>
2024-06-07 01:30:08 +0500

    -

--- pages/login.jsx
+++ pages/login.jsx
@@ -64,7 +64,7 @@ export default function Example() {
             } else if (data.role === "creator") {
               router.push("/creator");
             } else {
-              router.push("/");
+              router.push("/explore");
             }
           }, 1000);
         } else {
